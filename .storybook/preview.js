d27eb95e8087ab9bea3025bc4821495195b4ad96
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-03-24 15:19:31 -0300

    create: creating file for merging types in styled-components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,19 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider
-  }
+    nextRouter: {
+        Provider: RouterContext.Provider
+    }
 }
 
 export const decorators = [
-  (Story) => (
-    <>
-      <GlobalStyles />
-      <Story />
-    </>
-  )
+    (Story) => (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Story />
+        </ThemeProvider>
+    )
 ]
