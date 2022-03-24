b73e7400c62f40bc565835d4ce0dda6c6e78393c
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-03-24 11:53:46 -0300

    add: themes and fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,51 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
 
-  html {
-    font-size: 62.5%;
-  }
+    @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 300;
+        font-display: swap;
+        src: local(''),
+            url('/fonts/poppins-v19-latin-300.woff2') format('woff2')
+    }
+    @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: local(''),
+            url('/fonts/poppins-v19-latin-regular.woff2') format('woff2')
+    }
+    @font-face {
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 600;
+        font-display: swap;
+        src: local(''),
+            url('/fonts/poppins-v19-latin-600.woff2') format('woff2')
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+    html {
+        font-size: 62.5%;
+    }
+
+    html, body, #__next {
+        height: 100%;
+    }
+
+    body {
+        font-family: Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    }
 `
 
 export default GlobalStyles
