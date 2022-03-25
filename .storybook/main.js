f612c2b60cd84da16f15b5c8b248f61ca8051e1e
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-03-24 21:51:26 -0300

    add: adding logo component with tests for he

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
     staticDirs: ['../public'],
-    stories: ['../src/components/**/stories.tsx'],
+    stories: ['../src/components/**/*.stories.tsx'],
     addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
     core: {
         builder: 'webpack5'
