edaff5c037c3d11b61561790395d805751c24ecb
João Marcos <joomarcos.belanga98@gmail.com>
2022-05-31 18:00:07 -0300

    chore: tests for the menu

--- src/components/Menu/index.test.tsx
+++ src/components/Menu/index.test.tsx
@@ -1,13 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper/renderWithTheme'
 
 import Menu from '.'
 
 describe('<Menu />', () => {
-    it('should render the heading', () => {
-        const { container } = render(<Menu />)
+    it('should render the menu', () => {
+        renderWithTheme(<Menu />)
 
-        expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
+        expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+        expect(screen.getByRole("img", { name: /won games/i })).toBeInTheDocument()
+        expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    })
 
-        expect(container.firstChild).toMatchSnapshot()
-  })
+    it('should handle the open/close mobile menu', () => {
+        renderWithTheme(<Menu />)
+
+        const fullMenuElement = screen.getByRole("navigation", { hidden: true })
+        const openMenuOpen = screen.getByLabelText(/open menu/i)
+        const closeMenuButton = screen.getByLabelText(/close menu/i)
+
+        expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true")
+        expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+        fireEvent.click(openMenuOpen)
+        expect(fullMenuElement.getAttribute("aria-hidden")).toBe("false")
+        expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+        fireEvent.click(closeMenuButton)
+
+        expect(fullMenuElement.getAttribute("aria-hidden")).toBe("true")
+        expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+    })
+
+
 })
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,6 +1,11 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+
+type MenuFullProps = {
+    isOpen: boolean
+}
+
 export const Wrapper = styled.menu`
     ${({ theme }) => css`
         display: flex;
@@ -32,5 +37,36 @@ export const MenuGroup = styled.div`
         flex-grow: 1;
         justify-content: flex-end;
         gap: ${theme.spacings.xsmall};
+    `}
+`
+
+export const MenuNav = styled.div``
+
+export const MenuLink = styled.a``
+
+export const MenuFull = styled.nav<MenuFullProps>`
+    ${({ isOpen, theme }) => css`
+        background-color: ${theme.colors.white};
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 100vh;
+
+        overflow: hidden;
+        opacity: ${isOpen ? 1 : 0};
+        pointer-events: ${isOpen ? "all" : "none"};
+
+        > svg {
+            position: absolute;
+            top: 0;
+            right: 0;
+            margin: ${theme.spacings.xsmall};
+            width: 2.4rem;
+            height: 2.4rem;
+
+            cursor: pointer;
+        }
     `}
 `
