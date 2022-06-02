721a5bcf45780d45e21330462da1e1fc3e4d9bf0
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-02 16:35:53 -0300

    new: cretae snapshot for banner :sparkles: :heavy_check_mark:

--- src/components/Banner/index.test.tsx
+++ src/components/Banner/index.test.tsx
@@ -13,13 +13,14 @@ const props = {
 
 describe('<Banner />', () => {
     it('should render the heading', () => {
-        renderWithTheme(<Banner {...props} />)
+        const { container } = renderWithTheme(<Banner {...props} />)
 
         expect(screen.getByRole('heading', { name: /any title/i })).toBeInTheDocument()
         expect(screen.getByRole('heading', { name: /any subtitle/i })).toBeInTheDocument()
-        expect(screen.getByRole('img')).toHaveAttribute("src",
+        expect(screen.getByRole('img', { name: "any title" })).toHaveAttribute("src",
             'https://avatars.githubusercontent.com/u/72817686?v=4'
         )
+        expect(container.firstChild).toMatchSnapshot()
 
     })
 })
