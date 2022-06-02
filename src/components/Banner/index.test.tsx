3b00042d64a1d9c3fa96432e94baf5d85f7d1661
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-02 11:47:53 -0300

    new: create tests for Banner :heavy_check_mark:

--- src/components/Banner/index.test.tsx
+++ src/components/Banner/index.test.tsx
@@ -1,13 +1,23 @@
 import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper/renderWithTheme'
 
 import Banner from '.'
 
 describe('<Banner />', () => {
     it('should render the heading', () => {
-        const { container } = render(<Banner />)
+        renderWithTheme(<Banner
+            img='https://avatars.githubusercontent.com/u/72817686?v=4'
+            title='any title'
+            subtitle='any subtitle'
+            buttonLabel='any label'
+            buttonLink='any ref'
+        />)
 
-        expect(screen.getByRole('heading', { name: /Banner/i })).toBeInTheDocument()
+        expect(screen.getByRole('heading', { name: /any title/i })).toBeInTheDocument()
+        expect(screen.getByRole('heading', { name: /any subtitle/i })).toBeInTheDocument()
+        expect(screen.getByRole('img')).toHaveAttribute("src",
+            'https://avatars.githubusercontent.com/u/72817686?v=4'
+        )
 
-        expect(container.firstChild).toMatchSnapshot()
-  })
+    })
 })
