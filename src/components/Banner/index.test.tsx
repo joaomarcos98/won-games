12c70439f896f8caa8ec1ff36f6336457b4d1d5f
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-02 15:57:51 -0300

    chore: upgrade test for banner :hammer:

--- src/components/Banner/index.test.tsx
+++ src/components/Banner/index.test.tsx
@@ -3,15 +3,17 @@ import { renderWithTheme } from 'utils/tests/helper/renderWithTheme'
 
 import Banner from '.'
 
+const props = {
+    img: 'https://avatars.githubusercontent.com/u/72817686?v=4',
+    title: 'any title',
+    subtitle: '<strong>any subtitle<strong>',
+    buttonLabel: 'any label',
+    buttonLink: 'any ref'
+}
+
 describe('<Banner />', () => {
     it('should render the heading', () => {
-        renderWithTheme(<Banner
-            img='https://avatars.githubusercontent.com/u/72817686?v=4'
-            title='any title'
-            subtitle='any subtitle'
-            buttonLabel='any label'
-            buttonLink='any ref'
-        />)
+        renderWithTheme(<Banner {...props} />)
 
         expect(screen.getByRole('heading', { name: /any title/i })).toBeInTheDocument()
         expect(screen.getByRole('heading', { name: /any subtitle/i })).toBeInTheDocument()
