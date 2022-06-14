7d640228c1bb0bbea067b3613256192dd90d6ba5
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-14 11:01:23 -0300

    chore: refactoring tests of GameCardSlider :hammer:

--- src/components/GameCardSlider/index.test.tsx
+++ src/components/GameCardSlider/index.test.tsx
@@ -46,10 +46,17 @@ describe('<GameCardSlider />', () => {
     it('should render with four active items', () => {
         const { container } = renderWithTheme(<GameCardSlider items={items} />)
 
-        expect(container.querySelectorAll('.slick-slide')).toHaveLength(5)
         expect(container.querySelectorAll('div.slick-active')).toHaveLength(4)
-        expect(screen.getByRole('heading', { name: /Population Zero 1/i })).toBeInTheDocument()
-        expect(screen.getByRole('heading', { name: /Population Zero 4/i })).toBeInTheDocument()
-        expect(screen.queryByRole('heading', { name: /Population Zero 5/i })).not.toBeInTheDocument()
+    })
+
+    it('should render white arrow if color passed ', () => {
+        renderWithTheme(<GameCardSlider items={items} color='white' />)
+
+        expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+            color: '#FAFAFA'
+        })
+        expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+            color: '#FAFAFA'
+        })
     })
 })
