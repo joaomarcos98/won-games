7492f8ce69f9fc0acd0c6576f1d07f58eb6e09f3
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-17 16:26:51 -0300

    chore: create structure of Home template feeding with mocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMocks from 'components/BannerSlider/mock'
+import highLightMocks from 'components/Highlight/mock'
+import GamesMocks from 'components/GameCardSlider/mock'
 
 export default function Index(props: HomeTemplateProps) {
     return <Home {...props} />
@@ -7,7 +10,15 @@ export default function Index(props: HomeTemplateProps) {
 export function getServerSideProps() {
     return {
         props: {
-
+            banners: bannersMocks,
+            newGames: GamesMocks,
+            mostPopularHighlight: highLightMocks,
+            mostPopularGames: GamesMocks,
+            upcommingGames: GamesMocks,
+            upcommingHighlight: highLightMocks,
+            upcommingMoreGames: GamesMocks,
+            freeGames: GamesMocks,
+            freeGameHighLight: highLightMocks,
         }
     }
 }
