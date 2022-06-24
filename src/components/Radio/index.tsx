2093974269b8565d2279ede65dceafee5f860f39
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-24 17:47:44 -0300

    new: create Radio Component :sparkles:

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,40 @@
+import { InputHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type RadioValue = string | ReadonlyArray<string> | number
+
+export type RadioProps = {
+    onCheck?(value: RadioValue): void
+    label?: string
+    labelColor?: 'white' | 'black'
+    labelFor?: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Radio = ({ label, onCheck, labelColor = 'white', labelFor = '', value, ...rest }: RadioProps) => {
+
+    const onChange = () => {
+        !!onCheck && onCheck(value!)
+    }
+
+    return (
+        <S.Wrapper>
+            <S.Input
+                id={labelFor}
+                type='radio'
+                value={value}
+                onChange={onChange}
+                {...rest}
+            />
+            {!!label &&
+                <S.Label
+                    htmlFor={labelFor}
+                    labelColor={labelColor}
+                >
+                    {label}
+                </S.Label>
+            }
+        </S.Wrapper>
+    )
+}
+
+export default Radio
