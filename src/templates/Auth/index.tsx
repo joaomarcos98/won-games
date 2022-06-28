868b51d931df9d333713db089ae1b215f5252560
João Marcos <joomarcos.belanga98@gmail.com>
2022-06-28 10:36:14 -0300

    chore: add styles for Auth template :art:

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -11,20 +11,25 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
     <S.Wrapper>
         <S.BannerBlock>
-            <Logo />
-            <Heading>All your favorites games in one place</Heading>
-            <S.SubTitle>
-                <strong>WON</strong>
-                is the best and most complete gaming platform
-            </S.SubTitle>
-            <S.Footer>WON GAMES 2022 © Todos os Direitos Reservados</S.Footer>
+            <S.BannerContent>
+                <Logo />
+                <div>
+                    <Heading>All your favorites games in one place</Heading>
+                    <S.SubTitle>
+                        <strong>WON</strong> is the best and most complete gaming platform
+                    </S.SubTitle>
+                </div>
+                <S.Footer>WON GAMES 2022 © Todos os Direitos Reservados</S.Footer>
+            </S.BannerContent>
         </S.BannerBlock>
         <S.Content>
-            <Logo size="large" color="black" />
-            <Heading color="black" lineColor="secondary" lineLeft>
-                {title}
-            </Heading>
-            {children}
+            <S.ContentWrapper>
+                <Logo size="large" color="black" />
+                <Heading color="black" lineColor="secondary" lineLeft>
+                    {title}
+                </Heading>
+                {children}
+            </S.ContentWrapper>
         </S.Content>
     </S.Wrapper>
 )
